fb4324b6c50ff193d978bad262de3214bf0eb783
nikec <nikec.job@gmail.com>
2024-10-31 11:59:33 +0100

    migrate to nuqs v2

--- src/lib/params/controls.params.ts
+++ src/lib/params/controls.params.ts
@@ -1,10 +1,9 @@
+import { useQueryStates, UseQueryStatesKeysMap } from "nuqs";
 import {
   parseAsBoolean,
   parseAsNumberLiteral,
   parseAsStringLiteral,
-  useQueryStates,
-  UseQueryStatesKeysMap,
-} from "nuqs";
+} from "nuqs/server";
 
 import { EXPORT_SIZES } from "~/lib/const/export-size.const";
 import { PADDING_OPTIONS } from "~/lib/const/padding.const";
@@ -12,12 +11,14 @@ import { THEME_KEYS } from "~/lib/const/theme.const";
 
 export const CONTROLS_SEARCH_PARAMS = {
   theme: parseAsStringLiteral(THEME_KEYS).withDefault("graphite"),
-  background: parseAsBoolean.withDefault(true),
+  showBackground: parseAsBoolean.withDefault(true),
   darkMode: parseAsBoolean.withDefault(true),
   padding: parseAsNumberLiteral(PADDING_OPTIONS).withDefault(64),
-  size: parseAsNumberLiteral(EXPORT_SIZES).withDefault(4),
+  exportSize: parseAsNumberLiteral(EXPORT_SIZES).withDefault(6),
 } satisfies UseQueryStatesKeysMap;
 
 export function useControls() {
-  return useQueryStates(CONTROLS_SEARCH_PARAMS, { clearOnDefault: true });
+  return useQueryStates(CONTROLS_SEARCH_PARAMS, {
+    urlKeys: { showBackground: "background", exportSize: "size" },
+  });
 }
